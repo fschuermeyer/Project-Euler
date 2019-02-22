6040a5b303234f9ac26b21f17fee8f711c61658e
Yh9cacsr2KuuqyMxPxSc <fschuermeyer@gmail.com>
2019-02-22 18:57:47 +0100

    Update euler-5.py

--- euler-5.py
+++ euler-5.py
@@ -1,15 +1,14 @@
-k = False;
-i = 80;
+k = False
+i = 80
 while True:
     for b in range(1,20):
         if (i/b).is_integer() is not True:
-            k = True;
-            break;
+            k = True
+            break
 
     if k is not True:
         print(i)
-        break;
+        break
     else:
-        k = False;
+        k = False
     i += 80
-
